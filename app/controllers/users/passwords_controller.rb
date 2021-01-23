05501683e2ff824db6bba75cabac805b62ef0fa9
Hany Said <hany.said@rackspace.com>
2021-01-23 12:21:07 -0500

    make it better

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -91,10 +91,10 @@ class  Users::PasswordsController < Devise::PasswordsController
         print "*************** AWS  update unknown_error  PasswordsController  ***********************\n"
         flash[:alert] = I18n.t("devise.errors.unknown_error")
         #redirect_to edit_user_password_path(reset_password_token: params[:user][:reset_password_token])
-        redirect_to new_user_session_path  , :notice => 'password has been changed on AWS Cognito
+        redirect_to new_user_session_path  , :notice => 'password has been changed on AWS Cognito '
+
       end
       print "*************** AWS  update Done  PasswordsController  ***********************\n"
-   '
     end
   end
 
