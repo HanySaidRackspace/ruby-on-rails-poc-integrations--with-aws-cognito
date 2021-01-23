d63714161ad2c02099e5ec817b6d241d2723186c
Hany Said <hany.said@rackspace.com>
2021-01-22 20:43:08 -0500

    make it better

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -15,12 +15,12 @@ class UsersController < ApplicationController
       email: nil,
       authentication_hash: nil
     }
-    user = User.where(email: aws_auth_params[:email]).first
+    user = User.where(email: [:email]).first
 
     if user
       answer = user.as_json(only: defaults.keys)
       answer[:user_exists] = true
-      answer[:success] = user.valid_password?(aws_auth_params[:password])
+      answer[:success] = user.valid_password?([:password])
     else
       answer = defaults
       answer[:success] = false
