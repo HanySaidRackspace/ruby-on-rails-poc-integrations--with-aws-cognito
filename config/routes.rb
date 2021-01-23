1815f58f537035b45651c54c13fedecc2b0a397c
Hany Said <hany.said@rackspace.com>
2021-01-22 21:43:35 -0500

    make it better

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+Rails.application.routes.default_url_options[:host] = "XXX"
+
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
